9fdaa28210f4166c3f6ff564af4dbb0531b11128
kbaks9 <kamaal.bakar@outlook.com>
2026-02-03 22:13:44 +0000

    Set up architecture & modules & resources

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # WordPress-Deployment
+
+---
+
+### Assignment 1 - Deploy Wordpress
+
+Requirements:
+
+1. VM instance running WordPress
+2. Security groups - can only SSH in.
+3. User data/script/cloud-init to install dependencies.
--- main.tf
+++ main.tf
@@ -15,6 +15,21 @@ resource "azurerm_resource_group" "rg-wordpress" {
   location = var.location
 }
 
-# module "" {
-#   source = "./modules"
-# }
+module "compute" {
+  source              = "./modules/compute"
+  location            = var.location
+  resource_group      = var.resource_group
+  app_virtual_machine = var.app_virtual_machine
+  network_interface   = var.network_interface
+  subnet_id           = module.network.subnet
+  admin_user          = var.admin_user
+  admin_pass          = var.admin_pass
+}
+
+module "network" {
+  source               = "./modules/network"
+  location             = var.location
+  resource_group       = var.resource_group
+  virtual_network_name = var.virtual_network_name
+  subnet_name          = var.subnet_name
+}
