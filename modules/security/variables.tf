536a524da763a884b3dd06774c697651b0043b9c
kbaks9 <kamaal.bakar@outlook.com>
2026-02-03 22:16:47 +0000

    Setting up structure for network resources

---
+++ modules/security/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The location of the resources"
+  type        = string
+}
