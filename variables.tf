b38b26f1c25fe541d114993510decec2ec5f5224
kbaks9 <kamaal.bakar@outlook.com>
2026-02-03 22:13:57 +0000

    Setting all the variables in the root variables

--- variables.tf
+++ variables.tf
@@ -7,3 +7,37 @@ variable "location" {
   description = "The location of the resources"
   type        = string
 }
+
+### Compute
+
+variable "network_interface" {
+  description = "The name of the network interface"
+  type        = string
+}
+
+variable "app_virtual_machine" {
+  description = "The name of the virtual machine"
+  type        = string
+}
+
+variable "admin_user" {
+  description = "The admin username"
+  type        = string
+}
+
+variable "admin_pass" {
+  description = "The admin password"
+  type        = string
+}
+
+##### Network
+
+variable "virtual_network_name" {
+  description = "The name of the virtual network"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "The name of the subnet"
+  type        = string
+}
