d7dd16e79cbb5ff9b2d75f1d4961968a63f16e5b
kbaks9 <kamaal.bakar@outlook.com>
2026-02-05 22:02:48 +0000

    Added module variables now

--- main.tf
+++ main.tf
@@ -8,28 +8,31 @@ terraform {
 }
 provider "azurerm" {
   features {}
+
+  subscription_id = var.subscription_id
 }
 
-resource "azurerm_resource_group" "rg-wordpress" {
+resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group
   location = var.location
 }
 
 module "compute" {
   source              = "./modules/compute"
-  location            = var.location
-  resource_group      = var.resource_group
+  location            = azurerm_resource_group.resource_group.location
+  resource_group      = azurerm_resource_group.resource_group.name
   app_virtual_machine = var.app_virtual_machine
   network_interface   = var.network_interface
-  subnet_id           = module.network.subnet
+  subnet_id           = module.network.subnet_id
   admin_user          = var.admin_user
   admin_pass          = var.admin_pass
+  ssh_public_key      = "~/.ssh/wordpress_vm.pub"
 }
 
 module "network" {
   source               = "./modules/network"
-  location             = var.location
-  resource_group       = var.resource_group
+  location             = azurerm_resource_group.resource_group.location
+  resource_group       = azurerm_resource_group.resource_group.name
   virtual_network_name = var.virtual_network_name
   subnet_name          = var.subnet_name
 }
