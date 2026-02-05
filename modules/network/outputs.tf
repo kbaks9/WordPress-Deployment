a313fde8aabe80e4ff79f0ad09ec52375b3c9a47
kbaks9 <kamaal.bakar@outlook.com>
2026-02-05 23:34:09 +0000

    Added multiple outputs

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -2,3 +2,13 @@ output "subnet_id" {
   description = "ID of the internal subnet"
   value       = azurerm_subnet.subnetA.id
 }
+
+output "public_ip_id" {
+  description = "ID of the public IP"
+  value       = azurerm_public_ip.vm_public_ip.id
+}
+
+output "public_ip_address" {
+  description = "The actual public IP address"
+  value       = azurerm_public_ip.vm_public_ip.ip_address
+}
